0645ca7b89028f8813bff15c0b088c1282a3362b
Vitaly <kaliummati@gmail.com>
2021-07-01 04:19:25 +0900

    update

--- core/schedule/schedule.query.js
+++ core/schedule/schedule.query.js
@@ -1,13 +1,18 @@
 import { getUserHelper, clearMessageAndOnceEventsHepler } from '../../lib/telegram/index.js';
 import { setCacheMessageIdsHelper, setCacheDataHelper, getCacheDataHelper } from '../../lib/cache/index.js';
+import { scheduleService } from './schedule.service.js';
 
 export const scheduleQuery = async function(ctx) {
   const { id } = getUserHelper(ctx);
   await clearMessageAndOnceEventsHepler(this, id);
-
   const managerTelegramId = getCacheDataHelper(id).managerTelegramId;
   const dayName = ctx.message.text;
+  const scheduleId = await scheduleService.getSheduleId(managerTelegramId, dayName);
+  setCacheDataHelper(id, { scheduleId });
+  const times = await scheduleService.getTimesByManagerTelegramIdAndDayName(managerTelegramId, dayName);
 
-  console.log(managerTelegramId);
-  console.log(dayName);
+  let timesText = times.reduce((akk, time, index) => `${akk}${time.}`, 'Время отсылок:\n');
+
+  let msgId1 = (await this.sendMessage(id, dayName)).message_id;
+
 }
--- core/schedule/schedule.service.js
+++ core/schedule/schedule.service.js
@@ -5,9 +5,19 @@ const { PrismaClient } = Prisma;
 class ScheduleService {
   constructor() { this.db = new PrismaClient() }
 
-  async getTimesByManagerTelegramIdAndDayName() {
-    
+  async getTimesByManagerTelegramIdAndDayName(managerTelegramId, dayName) {
+    const manager = await this.db.manager.findUnique({ where: { telegram_id: managerTelegramId }, include: { schedules: true } });
+    let schedule = manager.schedules.find(schedule => schedule.dayName === dayName);
+    const times = (await this.db.schedule.findUnique({ where: { id: schedule.id }, include: { times: true } })).times;
+    return times;
   }
+
+  async getSheduleId(managerTelegramId, dayName) {
+    const manager = await this.db.manager.findUnique({ where: { telegram_id: managerTelegramId }, include: { schedules: true } });
+    let schedule = manager.schedules.find(schedule => schedule.dayName === dayName);
+    return schedule.id;
+  }
+
 }
 
 export const scheduleService = new ScheduleService();
